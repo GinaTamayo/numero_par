825f6d5560be1a3d4f37639158be64e9eee2aba9
Gina Tamayo <marcetamayo18@gmail.com>
2023-03-03 22:43:30 -0500

    Prueba commit

---
+++ Numero/numeroPar.js
@@ -0,0 +1,9 @@
+let numero = parseFloat(prompt("Ingrese el numero"));
+switch (numero / 2 == 0) {
+    case true:
+        console.log(numero , "es par")
+        break;
+  default:
+        console.log(numero , " es impar")
+        break;
+}
---
+++ numero con condicionales/numeroPar.js
@@ -0,0 +1,6 @@
+let numero = parseFloat(prompt("Ingrese el numero"));
+if (numero % 2 == 0) {
+    console.log(numero , "es par")
+} else {
+    console.log(numero , "es impar") 
+}       
